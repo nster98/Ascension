22f7d662d0a87cd059751d8ca7927b4e4732dfca
Nathan Glikman <nster98@gmail.com>
2019-12-28 02:54:34 -0500

    Input and Collision detection

---
+++ Ascension/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset.y = transform.position.y - player.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        transform.position = (new Vector3(transform.position.x, player.transform.position.y + offset.y, transform.position.z));
+    }
+}
--- Ascension/Assets/Scripts/PlayerControl.cs
+++ Ascension/Assets/Scripts/PlayerControl.cs
@@ -4,23 +4,74 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public SpriteRenderer render;
     public float speedY;
+    public float jumpSpeed;
+
+    private bool isMoving;
+
+    private enum Side {Left, Right};
+    private Side side;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        isMoving = false;
+
+        side = Side.Left;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Code for input
+        // Touch to move to the other side
+        // Swipe in the direction of movement to swing on the thingy
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                isMoving = true;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMoving = true;
+        }
     }
 
     // Physics here
     private void FixedUpdate()
     {
-        transform.Translate(Vector2.up * Time.deltaTime * speedY);   
+        transform.Translate(Vector2.up * Time.deltaTime * speedY);
+
+        if (isMoving == true && side == Side.Left)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * jumpSpeed);
+            render.flipX = true;
+        }
+        else if (isMoving == true && side == Side.Right)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * jumpSpeed);
+            render.flipX = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided");
+        if (collision.gameObject.tag == "Wall" && transform.position.x < 0)
+        {
+            isMoving = false;
+            side = Side.Left;
+        }
+        if (collision.gameObject.tag == "Wall" && transform.position.x > 0)
+        {
+            isMoving = false;
+            side = Side.Right;
+        }
     }
 }
